6c7520a00ebea18236f9fc606058201a5a75cce8
Jack Dao <jack@rubify.com>
2016-03-11 14:24:41 +0700

    detect some special emoji

--- Sample/RemoveEmojiSwift/RemoveEmojiSwift/ViewController.swift
+++ Sample/RemoveEmojiSwift/RemoveEmojiSwift/ViewController.swift
@@ -20,19 +20,20 @@ class ViewController: UIViewController {
         // you can see emoji with this code and filter with your custom
 //        let emojiRanges = [
 //            0x1D000...0x1F9C0
-//            //0x2100...0x3299
+////            0x2100...0x3299
 //        ]
 //        var stringemoji = ""
 //        for range in emojiRanges {
 //            for i in range {
 //                let c = Character(UnicodeScalar(i))
 //                stringemoji.append(c)
+//                stringemoji += "\(i)"
 //                stringemoji += String(format: "%2X  ",i)
 //            }
 //        }
 //        print(stringemoji)
         
-        let stringText = "🇦🆆 🅢 🄗 🄄 🀖 𝌺 𝄡 𝄞 𝂩 𝀭 QWERTYUIOP{}|\":LKJHYGTFRDESAZXCVB NM<>?   🌄  🌅  🌆  🌇  🌈  🌉  🌊  🌋  🌌  🌍  🌎  🌏  🌐  🌑  🌒  🌓  🌔  🌕  🌖  🌗  🌘  🌙  🌚  🌛  🌜  🌝  🌞  🌟  🌠  🌡  🌢  🌣  🌤  🌥  🌦  🌧  🌨  🌩  🌪  🌫  🌬  🌭  🌮  🌯  🌰  🌱  🌲  🌳  🌴  🌵  🌶  🌷  🌸  🌹  🌺  🌻  🌼  🌽  🌾  🌿  🍀  🍁  🍂  🍃  🍄  🍅  🍆  🍇  🍈  🍉  🍊  🍋  🍌  🍍  🍎  qtyuiop[]\\asdfghjkl;'zxcvbnm,./ 🅷 `1234567890-=~!@#$%^&*()_+ CHINE-有一天，我们开发什么 FRENCH-un jour, nous développons rien   😇  😈  😉  😊  😋  😌  😍  😎  😏  😐  😑  😒  😓  😔  😕  😖  😗  😘  😙 🚊 🚎 JAPANESE-ある日、私たちは何を開発 🔕 📡 💘 🐾  KOREAN-일일 우리는 아무것도 개발  🄧 🎬 🄨 arabic-يوم واحد ونحن نطور شيئا "// + stringemoji
+        let stringText = "🇦🆆 ㊙️ ㊗️ 〽️ 🅢 🄗 🄄 🀖 𝌺 𝄡 𝄞 𝂩 𝀭 QWERTYUIOP{}|\":LKJHYGTFRDESAZXCVB NM<>?   🌄  🌅  🌆  🌇  🌈  🌉  🌊  🌋  🌌  🌍  🌎  🌏  🌐  🌑  🌒  🌓  🌔  🌕  🌖  🌗  🌘  🌙  🌚  🌛  🌜  🌝  🌞  🌟  🌠  🌡  🌢  🌣  🌤  🌥  🌦  🌧  🌨  🌩  🌪  🌫  🌬  🌭  🌮  🌯  🌰  🌱  🌲  🌳  🌴  🌵  🌶  🌷  🌸  🌹  🌺  🌻  🌼  🌽  🌾  🌿  🍀  🍁  🍂  🍃  🍄  🍅  🍆  🍇  🍈  🍉  🍊  🍋  🍌  🍍  🍎  qtyuiop[]\\asdfghjkl;'zxcvbnm,./ 🅷 `1234567890-=~!@#$%^&*()_+ CHINE-有一天，我们开发什么 FRENCH-un jour, nous développons rien   😇  😈  😉  😊  😋  😌  😍  😎  😏  😐  😑  😒  😓  😔  😕  😖  😗  😘  😙 🚊 🚎 JAPANESE-ある日、私たちは何を開発 🔕 📡 💘 🐾  KOREAN-일일 우리는 아무것도 개발  🄧 🎬 🄨 arabic-يوم واحد ونحن نطور شيئا "// + stringemoji
         
         label.text = stringText
     }
