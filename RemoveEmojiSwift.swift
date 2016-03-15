fa0e82d54e7ce7b7ba859a5c7c186ae4fa7730e4
Jack Dao <jack@rubify.com>
2016-03-15 13:28:40 +0700

    update file master

--- RemoveEmojiSwift.swift
+++ RemoveEmojiSwift.swift
@@ -10,6 +10,9 @@ import Foundation
 
 extension String {
     func stringByRemoveEmoji() -> String {
+        if self.characters.count == 0 {
+            return self
+        }
         var resultString:String = ""
         for char in self.characters {
             if !char.isEmoji() {
@@ -18,6 +21,15 @@ extension String {
         }
         return resultString
     }
+    
+    func isHasEmoji() -> Bool {
+        for char in self.characters {
+            if char.isEmoji(){
+                return true
+            }
+        }
+        return false
+    }
 }
 
 extension Character {
@@ -25,8 +37,8 @@ extension Character {
         // remove emoji
         return Character(UnicodeScalar(0x1D000)) <= self && self <= Character(UnicodeScalar(0x1D3FF))
             || Character(UnicodeScalar(0x1D800)) <= self && self <= Character(UnicodeScalar(0x1F9C0))
+            || "〽️" == self  || self == "㊙️" || self == "㊗️"
             // remove symboy
-            || Character(UnicodeScalar(0x218A)) <= self && self <= Character(UnicodeScalar(0x26FF))
             || Character(UnicodeScalar(0x218A)) <= self && self <= Character(UnicodeScalar(0x2B59))
     }
 }
